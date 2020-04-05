027dd40089c1f2e2cf871343de289cf405845661
Pavel Krasnov <pavel.krasnov.work@gmail.com>
2020-04-05 12:23:58 +0100

    Complete task - change random instructor's random hobby

--- react/update-complex-state/src/App.js
+++ react/update-complex-state/src/App.js
@@ -21,6 +21,24 @@ class App extends Component {
         }
       ]
     };
+
+    const getRandomNumber = (maxNumberExcluded) => Math.floor(Math.random() * maxNumberExcluded);
+
+    setTimeout(() => {
+      const { instructors } = this.state;
+      const randomInstructorNumber = getRandomNumber(instructors.length);
+      console.log('Random instructor: ' + instructors[randomInstructorNumber].name);
+      const newInstructors = instructors.map((item, index) => {
+        if (index === randomInstructorNumber) {
+          const { hobbies } =  item; 
+          const randomHobbieNumber = getRandomNumber(hobbies.length);
+          console.log('Random hobbie: ' + hobbies[randomHobbieNumber]);
+          return {...item, hobbies: hobbies.slice(0, randomHobbieNumber).concat(hobbies.slice(randomHobbieNumber + 1))};
+        }
+        return item;
+      });
+      this.setState({...this.state, instructors: newInstructors});
+    }, 5000);
   }
   render() {
     const instructors = this.state.instructors.map((instructor, index) => (
